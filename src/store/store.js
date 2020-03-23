c3a032784e8dda4d54f78cf5d8aaf4f3fd58f947
Brendan Tanner <btan579@gmail.com>
2020-03-23 13:09:00 -0400

    formatting

--- src/store/store.js
+++ src/store/store.js
@@ -34,10 +34,6 @@ export const store = new Vuex.Store({
             return state.todosArr.filter(todo => !todo.completed).length;
         },
         anyRemainingTasks(state, getters) {
-            // if(){
-
-            // }
-            console.log(getters.remainingTodo);
             return getters.remainingTodo !== 0;
         },
         todosArrFiltered(state) {
